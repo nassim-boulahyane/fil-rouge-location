c07fc5fa5e4ed966a578b49a69eb0b76cf7476cd
nassim-boulahyane <nasshamid@hotmail.fr>
2022-11-29 22:20:29 +0100

    burger enfin

--- buttun.js
+++ buttun.js
@@ -2,7 +2,9 @@ function toggleMenu () {
     const menuburger = document.querySelector('.menuburger')
     const burger = document.querySelector('.burger')
     burger.addEventListener('click', () => {
-        menuburger.toggleAttribute("data-visible"); 
-    })
-}
+        menuburger.classList.toggle("visible");
+    });
+    
+};
+
 toggleMenu();
